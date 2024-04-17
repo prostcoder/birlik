b6a70f94e91479809a33494e61b561cdc2b203ce
mr-yeraliyev <yeraliyev.daryn@gmail.com>
2024-04-17 21:24:07 +0500

    copy from html

---
+++ src/views/LandingView.vue
@@ -0,0 +1,295 @@
+<template>
+  <div class="custom_header">
+    <ul class="custom_row custom_cent custom_half">
+      <li class="logo custom_row custom_cent">
+        <img src="../assets/Logo1.png" />
+        <h2>Бірлік</h2>
+      </li>
+      <div class="custom_row custom_cent custom_menu">
+        <li class="e1">клининг</li>
+        <li class="e2">макетирование</li>
+        <li class="e3">бухгалтерские услуги</li>
+        <li class="e4">мебель</li>
+      </div>
+    </ul>
+    <!--Главное меню. Если нажать одну из них, то прокрутит до определенного места.
+			Также в мобильной адаптации нажав на логотип Бирликб то появится меню.-->
+  </div>
+  <div class="main">
+    <div class="m2 custom_row custom_cent custom_rel">
+      <div class="i1 custom_rel ir gl"><img src="../assets/All.png" /></div>
+      <div class="custom_col right abs ART gr custom_text">
+        <div class="custom_col custom_cent">
+          <div class="logo custom_row custom_cent">
+            <img src="../assets/Logo1.png" />
+            <h2>Бірлік</h2>
+          </div>
+          <span>общественное объединение</span>
+          <span>инвалидов - предпринимателей</span>
+        </div>
+        <h1>Мы не видим границ, только возможности</h1>
+        <button class="gobut">Заказать консультацию</button>
+      </div>
+      <!--Главная страница. Нажав на кнопку появится ФИО, Проблемы и телефон.-->
+    </div>
+    <div class="m2 custom_row custom_cent custom_rel y1">
+      <div class="col">
+        <div class="custom_gt custom_text">
+          <h3>Клининг домов, квартир и офисов</h3>
+          <p>
+            Аутсорсинг клиниговых услуг отдельных офисов, а также целых
+            бизнес-центров с организацией комплексных клининговых процессов
+            всего здания.
+          </p>
+          <div class="t1 custom_col custom_cent gl">
+            <h4>Мы предоставим:</h4>
+            <div class="l custom_col custom_cent gl">
+              <div class="x xr custom_rowx custom_cent">
+                <img src="../assets/tube.png" />
+                <p>химчистка</p>
+              </div>
+              <div class="x xr custom_rowx custom_cent">
+                <img src="../assets/home.png" />
+                <p>генеральная уборка</p>
+              </div>
+              <div class="x xr custom_rowx custom_cent">
+                <img src="../assets/keep.png" />
+                <p>регулярная уборка</p>
+              </div>
+            </div>
+            <button class="gobut">Заказать консультацию</button>
+          </div>
+        </div>
+      </div>
+      <div class="i2 ir abs AR gr">
+        <img src="../assets/Clean.png" />
+      </div>
+    </div>
+    <div class="custom_col custom_rel y2">
+      <div class="m2 custom_col custom_cent">
+        <div class="custom_col custom_cent">
+          <div class="custom_col custom_cent custom_gt">
+            <h3>Макетирование</h3>
+            <p class="pc">
+              Полноцветная и высокоточная 3D-печать. Классическое изготовление
+              ваших архитектурных макетов
+            </p>
+            <h4 class="hx custom_hw">Процесс работы</h4>
+          </div>
+          <div class="custom_row custom_cent process custom_gt">
+            <div class="custom_col custom_cent">
+              <h4>Детали</h4>
+              <img src="../assets/details.png" />
+            </div>
+            <div class="custom_col custom_cent">
+              <h4>Ландшафт</h4>
+              <img src="../assets/lanshaft.png" />
+            </div>
+            <div class="custom_col custom_cent">
+              <h4>Продукт</h4>
+              <img src="../assets/maket.png" />
+            </div>
+          </div>
+          <div class="x1 custom_row custom_cent custom_rel custom_gt">
+            <h4>Нам доверяют проекты международного стандарта.</h4>
+            <img src="../assets/Naz.png" />
+            <span class="abs AR tr">ЖД Вокзал "Нұрлы жол"</span>
+          </div>
+          <button class="gobut">Заказать консультацию</button>
+        </div>
+      </div>
+      <div class="abs AT i3 irX red">
+        <img src="../assets/Piston.png" />
+      </div>
+    </div>
+    <div class="m2 custom_row custom_rel">
+      <div class="i4 custom_rel ir gl"><img src="../assets/Illustr.png" /></div>
+      <div class="custom_rel ART custom_col right custom_text y3">
+        <div class="t2 custom_gt">
+          <h3>Бухгалтерские услуги</h3>
+          <p>
+            От постановки бухгалтерского учета до полноценного ведения всей
+            бухгалтерской документации по доступной цене
+          </p>
+        </div>
+        <div class="l custom_col custom_cent gr">
+          <div class="x xt custom_rowx custom_cent">
+            <p>налоговое обслуживание</p>
+            <img src="../assets/Account.png" />
+          </div>
+          <div class="x xt custom_rowx custom_cent">
+            <p>автоматизация бухгалтерского учета</p>
+            <img src="../assets/Serv.png" />
+          </div>
+          <div class="x xt custom_rowx custom_cent">
+            <p>ведение бухгалтерского учёта</p>
+            <img src="../assets/Estimate.png" />
+          </div>
+          <div class="x xt custom_rowx custom_cent">
+            <p>налоговые консультации</p>
+            <img src="../assets/Consul.png" />
+          </div>
+          <button class="gobut">Заказать консультацию</button>
+        </div>
+      </div>
+    </div>
+    <div class="m2 custom_row custom_cent custom_rel">
+      <div class="custom_col custom_rel w1 custom_gt">
+        <h3>Корпусная мебель</h3>
+        <p>Современная мебель точно по вашим размерам</p>
+        <div class="i5 custom_rel custom_col">
+          <img class="exp" src="../assets/cloud.png" />
+          <div class="i6 custom_row abs">
+            <img class="exp" src="../assets/Deco.png" />
+          </div>
+          <div class="abs custom_rowx custom_cent i5x">
+            <div class="cl"><img src="../assets/L.png" /></div>
+            <div class="custom_black">
+              <div class="custom_rowx custom_cent">
+                <img class="exp" src="../assets/Picture.png" />
+                <img class="exp" src="../assets/1.jpg" />
+                <img class="exp" src="../assets/2.jpg" />
+                <img class="exp" src="../assets/3.jpg" />
+                <img class="exp" src="../assets/4.jpg" />
+                <img class="exp" src="../assets/5.jpg" />
+                <img class="exp" src="../assets/6.jpg" />
+                <img class="exp" src="../assets/7.jpg" />
+                <img class="exp" src="../assets/8.jpg" />
+                <img class="exp" src="../assets/9.jpg" />
+                <img class="exp" src="../assets/10.jpg" />
+                <img class="exp" src="../assets/11.jpg" />
+                <img class="exp" src="../assets/12.jpg" />
+                <img class="exp" src="../assets/13.jpg" />
+              </div>
+            </div>
+            <div class="cr"><img src="../assets/R.png" /></div>
+          </div>
+        </div>
+      </div>
+      <div class="custom_col custom_cent w2 gr">
+        <h3 class="h3l">У нас</h3>
+        <div class="custom_rowx custom_cent lx">
+          <div class="custom_col custom_cent">
+            <img src="../assets/low.png" />
+            <p>доступно</p>
+          </div>
+          <div class="custom_col custom_cent">
+            <img src="../assets/rating.png" />
+            <p>уникально</p>
+          </div>
+          <div class="custom_col custom_cent">
+            <img src="../assets/good.png" />
+            <p>качественно</p>
+          </div>
+        </div>
+        <div class="custom_rowx custom_cent">
+          <div class="green">
+            <img class="exp" src="../assets/Luxury.png" />
+          </div>
+          <div class="custom_col custom_cent h">
+            <div><img src="../assets/bed.png" /></div>
+            <div><img src="../assets/ward.png" /></div>
+            <div><img src="../assets/sofa.png" /></div>
+            <div><img src="../assets/food.png" /></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="m2 custom_col custom_cent red">
+      <button class="gobut">Заказать консультацию</button>
+    </div>
+    <div class="fix custom_col custom_cent">
+      <!--Появляется окно после нажатия "заказать консультацию" -->
+      <button class="close">X</button
+      ><!--Закрыть окно-->
+      <div class="col">
+        <p>ФИО</p>
+        <input type="text" name="" />
+      </div>
+      <div class="col">
+        <p>Ваши проблемы</p>
+        <input type="text" name="" />
+      </div>
+      <div class="col">
+        <p>Номер телефона</p>
+        <input type="number" name="" />
+      </div>
+      <div class="col">
+        <input class="gobut" type="submit" name="Отправить" />
+      </div>
+    </div>
+  </div>
+  <div class="custom_footer">
+    <div class="custom_row custom_cent">
+      <div class="custom_row custom_cent logo">
+        <img src="../assets/Logo1.png" />
+        <h2>Бірлік</h2>
+      </div>
+      <div>
+        <p class="px">«Общество Инвалидов –Предпринимателей «Бipлiк»</p>
+        <p class="px">БИН 141240026506</p>
+        <p class="px">ОКПО : 52807723</p>
+        <a href="birlik.pdf">Презентация</a>
+      </div>
+      <div>
+        <p class="px">«Банк ЦентрКредит»</p>
+        <p class="px">БИK : KCJBKZKX</p>
+        <p class="px">KZ988560000006963023</p>
+      </div>
+      <div>
+        <p class="px">Адрес: Республика Казахстан, 010000</p>
+        <p class="px">г. Астана, район Сарыарка</p>
+        <p class="px">пер.Маралды,д.18/1</p>
+        <p class="px">Тел/факс 8(7172)495989</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+/*Анимации*/
+// @keyframes wipe-enter {
+//   0% {
+//     transform: translateX(-100px);
+//   }
+//   100% {
+//     transform: translateX(0);
+//     opacity: 100%;
+//   } /*Слева*/
+// }
+// @keyframes wipe-enter2 {
+//   0% {
+//     transform: translateX(100px);
+//   }
+//   100% {
+//     transform: translateX(0);
+//     opacity: 100%;
+//   } /*Справа*/
+// }
+// @keyframes topX {
+//   0% {
+//     transform: translateY(50px);
+//   }
+//   100% {
+//     transform: translateY(0);
+//     opacity: 100%;
+//   } /*Сверху*/
+// }
+// @keyframes reduce {
+//   0% {
+//     transform: scaleX(1.5);
+//   }
+//   100% {
+//     transform: scaleX(1);
+//     opacity: 100%;
+//   } /*Уменьшение до нормального*/
+// }
+
+//.process,
+// .gt,
+// .gl,
+// .gr,
+// .red {
+//   opacity: 0%; /*Прозрачные до анимации*/
+// }
+</script>
